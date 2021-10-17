a6125b06766ef4c1bba74a909729ef16d70e0ab7
Piroev <epiroev@gmail.com>
2021-10-18 01:32:20 +0400

    filled inputs

--- src/EditUser.js
+++ src/EditUser.js
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 import users from "./users.json";
 
 function EditUser(props) {
-    
+
+    const [name, setName] = useState("");
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(name);
+    }
     return (
         <div className="popup">
-            <span className="formInputLabel">Name</span><input className="formInputs" type="text" name="name"></input>
-            <span className="formInputLabel">Last Name</span><input className="formInputs" type="text" name="lastname"></input>
-            <span className="formInputLabel">Email Address</span><input className="formInputs" type="email" name="email"></input>
-            <span className="formInputLabel">Gender</span><input className="formInputs" type="text" name="gender"></input>
-            <span className="formInputLabel">City</span><input className="formInputs" type="text" name="city"></input>
-            <span className="formInputLabel">Country</span><input className="formInputs" type="text" name="country"></input>
-            <input className="submit" type="submit"></input>
+            <form className="submitForm" onSubmit={handleSubmit}>
+                <span className="formInputLabel">Name</span><input className="formInputs" type="text" name="name" value={props.user.first_name} onChange={e => setName(e.target.value)}></input>
+                <span className="formInputLabel">Last Name</span><input className="formInputs" type="text" name="lastname" value={props.user.last_name}></input>
+                <span className="formInputLabel">Email Address</span><input className="formInputs" type="email" name="email" value={props.user.email}></input>
+                <span className="formInputLabel">Gender</span><input className="formInputs" type="text" name="gender" value={props.user.gender}></input>
+                <span className="formInputLabel">City</span><input className="formInputs" type="text" name="city" value={props.user.city}></input>
+                <span className="formInputLabel">Country</span><input className="formInputs" type="text" name="country" value={props.user.country}></input>
+                <input className="submit" type="submit"></input>
+            </form>
         </div>
     )
     
--- src/UsersList.js
+++ src/UsersList.js
@@ -17,11 +17,11 @@ function UsersList(){
             <ul>
                 {users.map( (user) => {
 
-                    return <li className="usersList" key = {user.id}><span className="infoLable">Name:</span><span className = "userInfo">{user.first_name}</span>
-                                                                     <span className="infoLable">Surname:</span><span className = "userInfo">{user.last_name}</span>
-                                                                     <span className="infoLable">E-mail:</span><span className = "userInfo">{user.email}</span>
-                                                                     <span className="infoLable">Gender:</span><span className = "userInfo">{user.gender}</span>
-                                                                     <span className="infoLable">City:</span><span className = "userInfo">{user.city}</span>
+                    return <li className="usersList" key = {user.id}><span className="infoLable">Name:</span><span className = "userInfo">{user.first_name} |</span>
+                                                                     <span className="infoLable">Surname:</span><span className = "userInfo">{user.last_name} |</span>
+                                                                     <span className="infoLable">E-mail:</span><span className = "userInfo">{user.email} |</span>
+                                                                     <span className="infoLable">Gender:</span><span className = "userInfo">{user.gender} |</span>
+                                                                     <span className="infoLable">City:</span><span className = "userInfo">{user.city} |</span>
                                                                      <span className="infoLable">Country:</span><span className = "userInfo">{user.country}</span>
                                                                      <button className="rowBtns" id="edit" onClick={(e) => {
                                                                                                                         e.preventDefault();
